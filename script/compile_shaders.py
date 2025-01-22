6b0c8ed443c4c5d6eebbc2e1a506ef660b9e7df4
noword <noword.forever@gmail.com>
2025-01-22 16:15:45 +0800

    update shader

--- script/compile_shaders.py
+++ script/compile_shaders.py
@@ -6,4 +6,4 @@ for name in Path('shaders').rglob('*.cg'):
     print(name)
     stem = name.stem
     os.system(f'psp2cgc.exe -profile sce_vp_psp2 -fastprecision -DVERTEX {name} -o {stem}_v.gxp')
-    os.system(f'psp2cgc.exe -profile sce_fp_psp2 -fastmath -DFRAGMENT {name} -o {stem}_f.gxp')
+    os.system(f'psp2cgc.exe -profile sce_fp_psp2 -fastprecision -DFRAGMENT {name} -o {stem}_f.gxp')
