b34ec3b3abeabefa1a5d0cfd12ed0f27ce53b5d8
noword <noword.forever@gmail.com>
2025-12-30 08:05:00 +0800

    reboot when load again for atari 5200

--- frontend/source/core_spec/defines.cpp
+++ frontend/source/core_spec/defines.cpp
@@ -117,7 +117,8 @@ const bool DEFAULT_MOUSE = CONFIG_MOUSE_DISABLE;
     defined(C64_BUILD) ||           \
     defined(X68000_BUILD) ||        \
     defined(MEDNAFEN_LYNX_BUILD) || \
-    defined(CPC_BUILD)
+    defined(CPC_BUILD) ||           \
+    defined(ATARI5200_BUILD)
 const bool DEFAULT_AUTO_SAVE = false;
 const bool DEFAULT_REBOOT_WHEN_LOADING_AGAIN = true;
 #else
