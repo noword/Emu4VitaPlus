3fed242ae6689d472ea9df30e8eaa67b102fc0f3
noword <noword.forever@gmail.com>
2026-01-28 08:38:01 +0800

    add 3 retroarch callbacks

--- frontend/source/emu/callbacks.cpp
+++ frontend/source/emu/callbacks.cpp
@@ -11,6 +11,37 @@
 
 #define CORE_OPTIONS_VERSION 2
 
+#include "performance.h"
+
+#define RETRO_ENVIRONMENT_RETROARCH_START_BLOCK 0x800000
+
+#define RETRO_ENVIRONMENT_SET_SAVE_STATE_IN_BACKGROUND (2 | RETRO_ENVIRONMENT_RETROARCH_START_BLOCK)
+/* bool * --
+ * Boolean value that tells the front end to save states in the
+ * background or not.
+ */
+
+#define RETRO_ENVIRONMENT_GET_CLEAR_ALL_THREAD_WAITS_CB (3 | RETRO_ENVIRONMENT_RETROARCH_START_BLOCK)
+/* retro_environment_t * --
+ * Provides the callback to the frontend method which will cancel
+ * all currently waiting threads.  Used when coordination is needed
+ * between the core and the frontend to gracefully stop all threads.
+ */
+
+#define RETRO_ENVIRONMENT_POLL_TYPE_OVERRIDE (4 | RETRO_ENVIRONMENT_RETROARCH_START_BLOCK)
+/* unsigned * --
+ * Tells the frontend to override the poll type behavior.
+ * Allows the frontend to influence the polling behavior of the
+ * frontend.
+ *
+ * Will be unset when retro_unload_game is called.
+ *
+ * 0 - Don't Care, no changes, frontend still determines polling type behavior.
+ * 1 - Early
+ * 2 - Normal
+ * 3 - Late
+ */
+
 void RetroLog(retro_log_level level, const char *fmt, ...)
 {
     // LogFunctionName;
@@ -50,6 +81,12 @@ void RetroLog(retro_log_level level, const char *fmt, ...)
     }
 }
 
+bool ClearAllThreadWaits(unsigned clear_threads, void *data)
+{
+    LogFunctionName;
+    return true;
+}
+
 bool EnvironmentCallback(unsigned cmd, void *data)
 {
     LogFunctionNameLimited;
@@ -57,6 +94,11 @@ bool EnvironmentCallback(unsigned cmd, void *data)
 
     switch (cmd)
     {
+    case RETRO_ENVIRONMENT_GET_CLEAR_ALL_THREAD_WAITS_CB:
+        LogDebug("  cmd: RETRO_ENVIRONMENT_GET_CLEAR_ALL_THREAD_WAITS_CB");
+        *(retro_environment_t *)data = ClearAllThreadWaits;
+        break;
+
     case RETRO_ENVIRONMENT_SET_ROTATION:
         LogDebug("  cmd: RETRO_ENVIRONMENT_SET_ROTATION");
         {
